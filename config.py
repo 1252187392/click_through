02dec68ddc3abc52a0e1b62538e58576b7e44a62
1252187392 <1252187392@qq.com>
2018-04-23 21:19:55 +0800

    参数更新

--- config.py
+++ config.py
@@ -17,13 +17,13 @@ TRAIN_SIZE = 0.95
 PROCESSES_FOR_CLEAN = 3
 
 #计算woe映射时的进程数
-PROCESSES_FOR_WOE = 4
+PROCESSES_FOR_WOE = 2
 
 #切分文件时,每PART_SIZE行切为一个文件
-PART_SIZE = 5000000
+PART_SIZE = 1000
 
-ORIGIN_TRAIN_FILE = 'origin_datas/train.csv'
-ORIGIN_TEST_FILE = 'origin_datas/test.csv'
+ORIGIN_TRAIN_FILE = 'origin_datas/part_train.csv'
+ORIGIN_TEST_FILE = 'origin_datas/part_test.csv'
 
 TRAIN_FILE = 'cache_data/use_train.csv'
 TEST_FILE = 'cache_data/use_test.csv'
@@ -35,6 +35,7 @@ WOE_TRAIN_LABLE = 'cache_data/woe_train_lables.npy'
 WOE_TEST_SAVE = 'cache_data/woe_test'
 WOE_TEST_FEATURE = 'cache_data/woe_test_features.npy'
 WOE_TEST_LABLE = 'cache_data/woe_test_lables.npy'
+WOE_HASH_INDEX = 'cache_data/woe_hash_index.npy'
 
 HASH_TRAIN_SAVE = 'cache_data/hash_train'
 HASH_TRAIN_FEATURE = 'cache_data/hash_train_features.npy'
